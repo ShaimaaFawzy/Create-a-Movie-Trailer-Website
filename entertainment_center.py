72d5109b4b6c92bced80d66d1ecb78dd2b5058d1
Unknown <unknown@example.com>
2018-10-25 10:16:07 +0200

    add the website files

---
+++ entertainment_center.py
@@ -0,0 +1,27 @@
+#import media class
+import media
+
+import fresh_tomatoes
+finding_nemo = media.Movie("Finding Nemo",
+	"Finding Nemo is a 2003 American computer-animated adventure film produced by Pixar Animation Studios",
+	"https://upload.wikimedia.org/wikipedia/en/2/29/Finding_Nemo.jpg",  
+	"https://www.youtube.com/watch?v=wZdpNglLbt8",
+        "8.1")  
+lion_king = media.Movie("The Lion King",
+	"The Lion King is a 1994 American animated epic musical film produced by Walt Disney Feature Animation and released ",
+	"https://upload.wikimedia.org/wikipedia/en/3/3d/The_Lion_King_poster.jpg",  
+	"https://www.youtube.com/watch?v=_ujGv5dhGfk",
+        "8.5")  
+
+
+inside_out  = media.Movie("Inside Out ",
+	"Inside Out is a 2015 American 3D computer-animated comedy-drama film produced by Pixar Animation Studios",
+	"https://upload.wikimedia.org/wikipedia/en/0/0a/Inside_Out_%282015_film%29_poster.jpg",  
+	"https://www.youtube.com/watch?v=seMwpP0yeu4",
+        "8.2")  
+
+# appending movies into a list:
+movies = [finding_nemo, lion_king, inside_out]
+
+# calling the external rendering function:
+fresh_tomatoes.open_movies_page(movies)
---
+++ media.py
@@ -0,0 +1,18 @@
+import webbrowser
+class Movie():
+     
+
+     def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube,film_rating):
+         self.title = movie_title
+         self.storyline = movie_storyline
+         self.poster_image_url = poster_image
+         self.trailer_youtube_url = trailer_youtube
+         self.rating=film_rating
+
+
+     def show_trailer(self):
+        """show_trailer() is for open the trailer url in the web brower
+        when the string run the web browser will be opened
+        """
+        webbrowser.open(self.trailer_youtube_url) 
+
